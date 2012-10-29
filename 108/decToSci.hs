fd6a6bba120faf37350e77a5a53bab6f372d27e8
Nick Knowlson <nick@nickknowlson.com>
2012-10-29 13:26:21 -0700

    started #108easy, put in basic command line calling structure

---
+++ 108/decToSci.hs
@@ -0,0 +1,21 @@
+import System.Environment
+import System.Random
+
+usage = "Usage: decToSci 340000\nor decToSci -g\nor decToSci -r \"3.2 * 10^2\""
+
+main = do
+    args <- getArgs
+    randomNum <- randomRIO (1, 2000000000) :: IO Int
+    case args of
+        [] -> putStrLn usage
+        "-g":_ -> putStrLn $ decToSci (show randomNum)
+        "-r":rest 
+            | length rest == 0 -> error usage
+            | otherwise -> putStrLn $ sciToDec $ head rest
+        argument:_ -> putStrLn $ decToSci argument
+
+decToSci :: String -> String
+decToSci num = num
+
+sciToDec :: String -> String
+sciToDec num = num
