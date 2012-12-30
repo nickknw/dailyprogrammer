609a795ba942be56d1395fc2d2b9cdccaa816dfe
Nick Knowlson <nick@nickknowlson.com>
2012-12-30 15:27:45 -0800

    do word ladder + most ladderable

--- 114/wordladder.hs
+++ 114/wordladder.hs
@@ -1,5 +1,8 @@
 import System.Environment
 import Control.Applicative
+import Data.String.Utils
+import Data.Ord
+import Data.List
 
 dictFile = "selected_four-letter_words.txt"
 
@@ -12,20 +15,32 @@ usage = "Usage:\n"
 
 main = do
     args <- getArgs
-    dict <- lines <$> readFile dictFile
+    dict <- lines <$> replace "\r" "" <$> readFile dictFile
     case args of
-        "list":word:[] -> wordLadder word dict
-        "most":_ -> mostLadderable dict
-        "chain":steps:start:[] -> wordChain steps start dict
+        "list":word:[] -> putStrLn $ fmtList $ wordLadder word dict
+        "most":_ -> putStrLn $ fmtList $ flattenPairs $ mostLadderable dict
+        "chain":steps:start:[] -> putStrLn $ wordChain steps start dict
         _ -> error usage
+
+fmtList list = foldl1 (\x acc-> x ++ "\n" ++ acc) list
+
+flattenPairs list = map (\p -> show (fst p) ++ " - " ++ show (snd p)) list
 
 -- Word Ladder
 
-wordLadder word dict = undefined
+wordLadder :: String -> [String] -> [String]
+wordLadder word dict = filter (oneLetterOff word) dict
+
+oneLetterOff :: String -> String -> Bool
+oneLetterOff word1 word2 = 1 == (sum $ map zeroOutMatches (zip word1 word2))
+    where zeroOutMatches (a, b) = if a == b then 0 else 1
 
 -- Most Ladderable
 
-mostLadderable dict = undefined
+mostLadderable dict =
+    let ladderLengths = map (\w -> (w, length (wordLadder w dict))) dict
+    in
+    take 10 $ reverse $ sortBy (comparing snd) ladderLengths
 
 -- Word Chain
 
