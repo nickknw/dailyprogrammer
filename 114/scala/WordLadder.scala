8d01ad94676fa4c1a0b7a9e619e8aaef467ba554
Nick Knowlson <nick@nickknowlson.com>
2013-03-19 13:30:38 -0700

    114scala - finished most ladderable and word chain, completing the exercise

--- 114/scala/WordLadder.scala
+++ 114/scala/WordLadder.scala
@@ -6,21 +6,37 @@ object WordLadder {
 
     // Word Ladder
 
-    def wordLadder(word: String, dict: Iterator[String]): Seq[String] = {
-        return List("hello")
-    }
+    def wordLadder(word: String, dict: Seq[String]): Seq[String] = 
+        dict.filter(oneLetterOff(word, _))
+
+
+    private def oneLetterOff(word1: String, word2: String): Boolean = 
+        (word1, word2).zipped.map(_ == _).filter(x => !x).length == 1
 
     // Most Ladderable
 
-    def mostLadderable(num: Int, dict: Iterator[String]): Seq[(String, Int)] = {
-        return List(("tmp", 1))
+    def mostLadderable(num: Int, dict: Seq[String]): Seq[(String, Int)] = {
+        val wordNumberPairs = for (w <- dict) yield (w, wordLadder(w, dict).length)
+        wordNumberPairs.sortBy(_._2).reverse.take(num)
     }
 
     // Word Chain
 
-    def wordChain(steps: Int, current: String, dict: Iterator[String]): Seq[String] = {
-        return List("tmp")
+    def wordChain(steps: Int, current: String, dict: Seq[String]): Set[String] = {
+        var finalSet = Set(current)
+        var next = nextLevelOfWords(finalSet, dict)
+
+        for (i <- 0 until steps) {
+            finalSet = finalSet ++ next
+            next = nextLevelOfWords(next, dict)
+        }
+
+        return finalSet
     }
+
+    private def nextLevelOfWords(current: Set[String], dict: Seq[String]): Set[String] = 
+        current.flatMap(w => wordLadder(w, dict))
+
 
     // Command line interface
 
@@ -31,12 +47,12 @@ object WordLadder {
     def fmtPairs(pairs: Seq[(String, Int)]): String = pairs.flatMap(p => p._1 + " - " + p._2 + "\n").mkString
 
     def main(args: Array[String]): Unit = {
-        val dict = fromFile(dictFile).getLines
+        val dict = fromFile(dictFile).getLines.toSeq
 
         args toList match {
             case "list" :: word :: Nil            => println(fmtList(wordLadder(word, dict)))
             case "top" :: num :: Nil              => println(fmtPairs(mostLadderable(num.toInt, dict)))
-            case "chain" :: steps :: start :: Nil => println(wordChain(steps.toInt, start, dict).length)
+            case "chain" :: steps :: start :: Nil => println(wordChain(steps.toInt, start, dict).size)
             case _                                => println(usage)
         }
     }
